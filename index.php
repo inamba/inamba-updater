a57e994e4098c9f824dc73bcc01b8720a02df124
Inamba <esteban@inamba.com>
2023-04-26 10:06:16 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+	
+	/*
+	
+	@author:			Inamba
+	@updated:			2023-03-13 00:00
+	
+	*/
+	
+	header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+	header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+
+	header("Cache-Control: no-store, no-cache, must-revalidate");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
+
+	header("Location:https://github.com/inamba/updater/");
+	exit;
